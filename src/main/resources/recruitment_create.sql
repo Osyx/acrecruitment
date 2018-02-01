224f627981625f26bb8bf03cf54200b91f8e0473
Unknown <moaqb88@gmail.com>
2018-02-01 14:13:50 +0100

    changed sql script

--- src/main/resources/recruitment_create.sql
+++ src/main/resources/recruitment_create.sql
@@ -2,9 +2,9 @@
 -- Last modification date: 2018-01-29 10:52:02.892
 
 -- tables
--- Table: person
+-- Table: Person
 CREATE TABLE person (
-    person_id bigint NOT NULL,
+    person_id bigint NOT NULL auto_increment,
     name varchar(40) NOT NULL,
     surname varchar(40) NOT NULL,
     ssn varchar(40) NOT NULL,
@@ -14,23 +14,23 @@ CREATE TABLE person (
 
 -- Table: availability
 CREATE TABLE availability (
-    availability_id bigint NOT NULL,
+    availability_id bigint NOT NULL auto_increment,
     person_id bigint NOT NULL,
     from_date date NOT NULL,
-    to_date date NOT NULL,
+    to_date date,
     CONSTRAINT availability_pk PRIMARY KEY (availability_id)
 );
 
 -- Table: experience
 CREATE TABLE experience (
-    experience_id bigint NOT NULL,
+    experience_id bigint NOT NULL auto_increment,
     name varchar(40) NOT NULL,
     CONSTRAINT experience_pk PRIMARY KEY (experience_id)
 );
 
 -- Table: person_experience
 CREATE TABLE person_experience (
-    person_experience_id bigint NOT NULL,
+    person_experience_id bigint NOT NULL auto_increment,
     person_id bigint NOT NULL,
     experience_id bigint NOT NULL,
     years_of_experience double(4,2) NOT NULL,
@@ -46,14 +46,14 @@ CREATE TABLE person_role (
 
 -- Table: role
 CREATE TABLE role (
-    role_id bigint NOT NULL,
+    role_id bigint NOT NULL auto_increment,
     name varchar(40) NOT NULL,
     CONSTRAINT role_pk PRIMARY KEY (role_id)
 );
 
 -- Table: user
 CREATE TABLE user (
-    user_id bigint NOT NULL,
+    user_id bigint NOT NULL auto_increment,
     person_id bigint NOT NULL,
     username varchar(40) NOT NULL,
     password varchar(40) NOT NULL,
@@ -63,27 +63,26 @@ CREATE TABLE user (
 -- foreign keys
 -- Reference: availability_Person (table: availability)
 ALTER TABLE availability ADD CONSTRAINT availability_Person FOREIGN KEY availability_Person (person_id)
-    REFERENCES person (person_id);
+REFERENCES Person (person_id);
 
 -- Reference: person_experience_Person (table: person_experience)
 ALTER TABLE person_experience ADD CONSTRAINT person_experience_Person FOREIGN KEY person_experience_Person (person_id)
-    REFERENCES person (person_id);
+REFERENCES Person (person_id);
 
 -- Reference: person_experience_experience (table: person_experience)
 ALTER TABLE person_experience ADD CONSTRAINT person_experience_experience FOREIGN KEY person_experience_experience (experience_id)
-    REFERENCES experience (experience_id);
+REFERENCES experience (experience_id);
 
 -- Reference: person_role_Person (table: person_role)
 ALTER TABLE person_role ADD CONSTRAINT person_role_Person FOREIGN KEY person_role_Person (person_id)
-    REFERENCES person (person_id);
+REFERENCES Person (person_id);
 
 -- Reference: person_role_role (table: person_role)
 ALTER TABLE person_role ADD CONSTRAINT person_role_role FOREIGN KEY person_role_role (role_id)
-    REFERENCES role (role_id);
+REFERENCES role (role_id);
 
 -- Reference: user_Person (table: user)
 ALTER TABLE user ADD CONSTRAINT user_Person FOREIGN KEY user_Person (person_id)
-    REFERENCES person (person_id);
+REFERENCES Person (person_id);
 
 -- End of file.
-
