108282e18b4ad2d98366667bbabd2087a3c169e4
Oscar Falkman <ofalkman@gmail.com>
2018-03-05 12:37:59 +0100

    Added JavaDoc for new class.

--- src/main/java/viewmodel/response/Experience.java
+++ src/main/java/viewmodel/response/Experience.java
@@ -2,6 +2,10 @@ package viewmodel.response;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * Class used to be able to send the experience names to the REST client.
+ * Can be converted to a JSON/XML object.
+ */
 @XmlRootElement
 public class Experience {
     private String name;
